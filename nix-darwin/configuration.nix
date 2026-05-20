a6e8ef623b4653741a4ae6d2b1d02c96c5dad691
Chikuwa Saurus <285532703+chikuwasaurus@users.noreply.github.com>
2026-05-21 08:51:16 +0900

    Manage Nix settings with nix-darwin

--- nix-darwin/configuration.nix
+++ nix-darwin/configuration.nix
@@ -17,6 +17,14 @@
 
   nix.enable = false;
 
+  nix = {
+    package = pkgs.nix;
+    settings.experimental-features = [
+      "nix-command"
+      "flakes"
+    ];
+  };
+
   system = {
     stateVersion = 7;
 
