1dbb65683527d88b7b320cb8093751cbaaa234e5
Chikuwa Saurus <285532703+chikuwasaurus@users.noreply.github.com>
2026-05-20 14:02:05 +0900

    Set Home Manager backup file extension

--- nix-darwin/home-manager.nix
+++ nix-darwin/home-manager.nix
@@ -1,7 +1,11 @@
 # The flake-based setup of the Home Manager nix-darwin module.
 # https://nix-community.github.io/home-manager/index.xhtml#sec-flakes-nix-darwin-module
 {
-  home-manager.useGlobalPkgs = true;
-  home-manager.useUserPackages = true;
-  home-manager.users."kyohei" = ../home-manager/home.nix;
+  home-manager = {
+    useGlobalPkgs = true;
+    useUserPackages = true;
+    backupFileExtension = "backup";
+
+    users."kyohei" = ../home-manager/home.nix;
+  };
 }
