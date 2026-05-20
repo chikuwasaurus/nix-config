778e080436d64e91c207103e01794bcc371f19d0
Chikuwa Saurus <285532703+chikuwasaurus@users.noreply.github.com>
2026-05-20 14:56:06 +0900

    Fix nixConfigPath

--- home-manager/dotfiles.nix
+++ home-manager/dotfiles.nix
@@ -1,16 +1,16 @@
 { config, ... }:
 
 let
-  nixConfigPath = "${config.home.homeDirectory}/Developer";
+  nixConfigPath = "${config.home.homeDirectory}/Developer/nix-config";
   mkLink = path: config.lib.file.mkOutOfStoreSymlink "${nixConfigPath}/home-manager/${path}";
 in
 {
   # Home Manager is pretty good at managing dotfiles. The primary way to manage
   # plain files is through 'home.file'.
   home.file = {
-    ".gitconfig".source = mkLink ./git/.gitconfig;
-    ".zprofile".source = mkLink ./zsh/.zprofile;
-    ".zshrc".source = mkLink ./zsh/.zshrc;
+    ".gitconfig".source = mkLink "git/.gitconfig";
+    ".zprofile".source = mkLink "zsh/.zprofile";
+    ".zshrc".source = mkLink "zsh/.zshrc";
 
     # # Building this configuration will create a copy of 'dotfiles/screenrc' in
     # # the Nix store. Activating the configuration will then make '~/.screenrc' a
@@ -26,11 +26,11 @@ in
 
   # ~/.config
   xdg.configFile = {
-    "ghostty".source = mkLink ./ghostty;
-    "nushell".source = mkLink ./nushell;
-    "nvim".source = mkLink ./nvim;
-    "sheldon".source = mkLink ./sheldon;
-    "starship.toml".source = mkLink ./starship/starship.toml;
-    "zed".source = mkLink ./zed;
+    "ghostty".source = mkLink "ghostty";
+    "nushell".source = mkLink "nushell";
+    "nvim".source = mkLink "nvim";
+    "sheldon".source = mkLink "sheldon";
+    "starship.toml".source = mkLink "starship/starship.toml";
+    "zed".source = mkLink "zed";
   };
 }
