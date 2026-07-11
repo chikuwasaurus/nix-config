0bb3a715b22e924cd7f322b92c3d17efe63c4251
Chikuwa Saurus <285532703+chikuwasaurus@users.noreply.github.com>
2026-07-11 09:55:50 +0900

    Disable unsupported ZLE bindings in Herdr

--- home-manager/zsh/keybindings.zsh
+++ home-manager/zsh/keybindings.zsh
@@ -96,43 +96,43 @@ bindkey '^X^Y' copy-region-or-buffer-to-clipboard
 # Since Ctrl-e is bound to our wrapper instead of the built-in `end-of-line`,
 # register the wrapper as a whole-suggestion accept widget before loading
 # zsh-autosuggestions.
-ZSH_AUTOSUGGEST_ACCEPT_WIDGETS+=(move-end-of-line)
+# ZSH_AUTOSUGGEST_ACCEPT_WIDGETS+=(move-end-of-line)
 # Ctrl-f should still partially accept suggestions one character at a time.
-ZSH_AUTOSUGGEST_PARTIAL_ACCEPT_WIDGETS+=(move-forward-char)
+# ZSH_AUTOSUGGEST_PARTIAL_ACCEPT_WIDGETS+=(move-forward-char)
 
 # Move without selecting.
 # These clear the active ZLE region, matching macOS text-field behavior:
 # normal movement cancels selection, Shift+movement extends it.
 
-move-backward-char() {
-	zle deactivate-region
-	zle backward-char
-}
+# move-backward-char() {
+# 	zle deactivate-region
+# 	zle backward-char
+# }
 
-move-forward-char() {
-	zle deactivate-region
-	zle forward-char
-}
+# move-forward-char() {
+# 	zle deactivate-region
+# 	zle forward-char
+# }
 
-move-beginning-of-line() {
-	zle deactivate-region
-	zle beginning-of-line
-}
+# move-beginning-of-line() {
+# 	zle deactivate-region
+# 	zle beginning-of-line
+# }
 
-move-end-of-line() {
-	zle deactivate-region
-	zle end-of-line
-}
+# move-end-of-line() {
+# 	zle deactivate-region
+# 	zle end-of-line
+# }
 
-zle -N move-backward-char
-zle -N move-forward-char
-zle -N move-beginning-of-line
-zle -N move-end-of-line
+# zle -N move-backward-char
+# zle -N move-forward-char
+# zle -N move-beginning-of-line
+# zle -N move-end-of-line
 
-bindkey '^B' move-backward-char
-bindkey '^F' move-forward-char
-bindkey '^A' move-beginning-of-line
-bindkey '^E' move-end-of-line
+# bindkey '^B' move-backward-char
+# bindkey '^F' move-forward-char
+# bindkey '^A' move-beginning-of-line
+# bindkey '^E' move-end-of-line
 
 # Move while selecting the current ZLE buffer region.
 # These widgets make ZLE behave more like macOS text fields:
@@ -140,46 +140,46 @@ bindkey '^E' move-end-of-line
 #
 # see: ~/.config/ghostty/config.ghostty
 
-select-backward-char() {
-	((REGION_ACTIVE)) || zle set-mark-command
-	zle backward-char
-}
+# select-backward-char() {
+# 	((REGION_ACTIVE)) || zle set-mark-command
+# 	zle backward-char
+# }
 
-select-forward-char() {
-	((REGION_ACTIVE)) || zle set-mark-command
-	zle forward-char
-}
+# select-forward-char() {
+# 	((REGION_ACTIVE)) || zle set-mark-command
+# 	zle forward-char
+# }
 
-select-beginning-of-line() {
-	((REGION_ACTIVE)) || zle set-mark-command
-	zle beginning-of-line
-}
+# select-beginning-of-line() {
+# 	((REGION_ACTIVE)) || zle set-mark-command
+# 	zle beginning-of-line
+# }
 
-select-end-of-line() {
-	((REGION_ACTIVE)) || zle set-mark-command
-	zle end-of-line
-}
+# select-end-of-line() {
+# 	((REGION_ACTIVE)) || zle set-mark-command
+# 	zle end-of-line
+# }
 
-select-backward-word() {
-	((REGION_ACTIVE)) || zle set-mark-command
-	zle backward-word
-}
+# select-backward-word() {
+# 	((REGION_ACTIVE)) || zle set-mark-command
+# 	zle backward-word
+# }
 
-select-forward-word() {
-	((REGION_ACTIVE)) || zle set-mark-command
-	zle forward-word
-}
+# select-forward-word() {
+# 	((REGION_ACTIVE)) || zle set-mark-command
+# 	zle forward-word
+# }
 
-zle -N select-backward-char
-zle -N select-forward-char
-zle -N select-beginning-of-line
-zle -N select-end-of-line
+# zle -N select-backward-char
+# zle -N select-forward-char
+# zle -N select-beginning-of-line
+# zle -N select-end-of-line
 
-# Private escape sequences sent by the terminal.
-bindkey '\e[1;S-a' select-beginning-of-line
-bindkey '\e[1;S-e' select-end-of-line
-bindkey '\e[1;S-b' select-backward-char
-bindkey '\e[1;S-f' select-forward-char
+# # Private escape sequences sent by the terminal.
+# bindkey '\e[1;S-a' select-beginning-of-line
+# bindkey '\e[1;S-e' select-end-of-line
+# bindkey '\e[1;S-b' select-backward-char
+# bindkey '\e[1;S-f' select-forward-char
 
 # Use ^G only as the fzf-git.sh prefix.
 # see: ~/.config/sheldon/plugins.toml
