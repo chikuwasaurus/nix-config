3a4cf2ac34c919aa2b5d69e4bc516945f816f97a
Chikuwa Saurus <285532703+chikuwasaurus@users.noreply.github.com>
2026-07-22 23:34:57 +0900

    Configure dark themes for GTK and Qt apps in NixOS

--- home-manager/nixos.nix
+++ home-manager/nixos.nix
@@ -5,18 +5,18 @@ let
   mkLink = path: config.lib.file.mkOutOfStoreSymlink "${nixConfigPath}/home-manager/${path}";
 in
 {
-  # # Use a dark theme for GTK applications.
-  # gtk = {
-  #   enable = true;
-  #   colorScheme = "dark";
-  # };
+  # Use a dark theme for GTK applications.
+  gtk = {
+    enable = true;
+    colorScheme = "dark";
+  };
 
-  # # Use a dark theme for Qt applications.
-  # qt = {
-  #   enable = true;
-  #   platformTheme.name = "adwaita";
-  #   style.name = "adwaita-dark";
-  # };
+  # Use a dark theme for Qt applications.
+  qt = {
+    enable = true;
+    platformTheme.name = "adwaita";
+    style.name = "adwaita-dark";
+  };
 
   # Packages that should be installed to the user profile.
   home.packages = with pkgs; [
