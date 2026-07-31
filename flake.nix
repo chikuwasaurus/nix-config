5d73ffa3df7eeb2fd7acf735c4e68338ea284e77
Chikuwa Saurus <285532703+chikuwasaurus@users.noreply.github.com>
2026-07-31 10:42:36 +0900

    Add treefmt-nix

--- flake.nix
+++ flake.nix
@@ -46,6 +46,10 @@
       url = "github:nix-community/disko/latest";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+    treefmt-nix = {
+      url = "github:numtide/treefmt-nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
   outputs =
